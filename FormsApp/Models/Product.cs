ab55136953e581412ca8f25b1523d6ccbd38474d
Zeynel Abidin SARISALTIK <75449702+ZeynelAbidin1@users.noreply.github.com>
2025-02-26 16:57:39 +0300

    Add files via upload

---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+    public class Product 
+    {
+        [Display(Name= "Ürün Id")]
+        
+        public int ProductId {  get; set; }
+        [Required(ErrorMessage = "Ürün Adını Giriniz.")]
+        [Display(Name = "Ürün Adı")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage ="Fiyat Bilgisini Giriniz.")]
+        [Display(Name = "Fiyat")]
+        public decimal? Price { get; set; }
+        //[Required(ErrorMessage ="Ürün Görsel Adını Yazınız.Örn:1.jpg")]
+        [Display(Name = "Ürün Görseli")]
+        public string? Image{ get; set; }
+        public bool IsActive { get; set; }
+        [Required(ErrorMessage ="Ürünün Bulunduğu Kategoriyi Seçiniz.")]
+        [Display(Name = "Category")]
+        public int? CategoryId { get; set; }
+    }
+}
---
+++ FormsApp/Models/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _categories = new();
+
+        static Repository()
+        {
+            _categories.Add(new Category { CategoryId=1,Name ="Telefon" });
+            _categories.Add(new Category { CategoryId=2,Name ="Bilgisayar" });
+            _products.Add(new Product {ProductId=1,Name="Iphone 11", Price= 40000, IsActive=true, Image="11.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=2,Name="Iphone 12", Price= 50000, IsActive=false, Image="12.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=3,Name="Iphone 13", Price= 60000, IsActive=true, Image="13.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=4,Name="Iphone 14", Price= 70000, IsActive=false, Image="14.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=5,Name="Iphone 15", Price= 80000, IsActive=true, Image="15.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=6,Name="Iphone 16", Price= 90000, IsActive=false, Image="16.jpg",CategoryId=1});
+            _products.Add(new Product {ProductId=7,Name="MacBook 14\"", Price= 90000, IsActive=true, Image="m14.jpg",CategoryId=2});
+            _products.Add(new Product {ProductId=8,Name="MacBook 16\"", Price= 100000, IsActive=true, Image="m16.jpg",CategoryId=2});
+        }
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+        public static void CreateProduct(Product entity){
+            _products.Add(entity);
+        }
+
+        public static void EditProduct(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
+            if (entity != null)
+            {
+                entity.Name = updatedProduct.Name;
+                entity.Price = updatedProduct.Price;
+                entity.Image = updatedProduct.Image;
+                entity.CategoryId = updatedProduct.CategoryId;
+                entity.IsActive = updatedProduct.IsActive;
+            }
+        }
+        public static void EditIsActive(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
+            if (entity != null)
+            {
+                entity.IsActive = updatedProduct.IsActive;
+            }
+        }
+        public static void DeleteProduct(Product deletedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
+            if (entity != null)
+            {
+                _products.Remove(entity);
+            }
+        }
+
+        public static List<Category> Categories
+        {
+            get {
+                return _categories;
+            }
+        }
+
+    }
+
+}
